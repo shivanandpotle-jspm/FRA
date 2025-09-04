bafbc32c04bced172001fc885a9f76d19097ff49
shivanandpotle-jspm <shpotle_it@jspmrscoe.edu.in>
2025-09-05 01:25:24 +0530

    Update asset.js

--- Atlas/fra-atlas-dss/backend/src/models/asset.js
+++ Atlas/fra-atlas-dss/backend/src/models/asset.js
@@ -1,38 +1,44 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const db = require('../utils/db');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../utils/db");
 
 // Define the Asset model
-const Asset = db.define('Asset', {
+const Asset = sequelize.define(
+  "Asset",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     type: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     area: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
+      type: DataTypes.FLOAT,
+      allowNull: false,
     },
     classification: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     coordinates: {
-        type: DataTypes.GEOMETRY('POLYGON'),
-        allowNull: false,
+      type: DataTypes.GEOMETRY("POLYGON"),
+      allowNull: false,
     },
     createdAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
     },
     updatedAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
     },
-});
+  },
+  {
+    tableName: "assets", // explicit table name
+    timestamps: true, // automatically handles createdAt & updatedAt
+  }
+);
 
-// Export the Asset model
 module.exports = Asset;
